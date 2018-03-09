fd959007e906cb95c86133266b5f731357ef3bfb
daddabarba <davidebarbieri97@gmail.com>
2018-03-10 00:09:07 +0100

    facilitating action selection

--- simulation/simModel/agent/load_agent.py
+++ simulation/simModel/agent/load_agent.py
@@ -1,7 +1,16 @@
 import sys
 import agent
 
+def defInput(mes, defVal):
+    return int(input("(" + str(defVal) + ") - " + mes) or str(defVal))
+
 if len(sys.argv) != 0:
     a = agent.agent(environment=(sys.argv)[1])
 else:
     a = agent.agent()
+
+nSteps = 500
+
+while nSteps > 0:
+    nSteps = defInput("Insert number of steps: ", nSteps)
+    a.nSteps(nSteps,0)
