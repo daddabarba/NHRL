34d882377df3b77c3ccf733f18fc68fa2bb596ad
daddabarba <davidebarbieri97@gmail.com>
2018-03-28 15:52:57 +0200

    updated test parameters

--- simulation/simModel/agent/load_agent.py
+++ simulation/simModel/agent/load_agent.py
@@ -1,11 +1,28 @@
 import sys
 import agent
 
+env = None
+loop = True
+
 def defInput(mes, defVal):
     return int(input("(" + str(defVal) + ") - " + mes) or str(defVal))
 
-if len(sys.argv) > 1:
-    a = agent.agent(environment=(sys.argv)[1])
+def runPars(i):
+    if (sys.argv)[i] == "path":
+        env = (sys.argv)[i+1]
+    elif (sys.argv)[i] == "loop":
+        loop = True if (sys.argv)[i+1] == "True" else False
+
+
+if not len(sys.argv)%2:
+    print "wrong argument format"
+    sys.exit()
+
+for i in range(1,len(sys.argv)-1,2):
+    runPars(i)
+
+if env:
+    a = agent.agent(environment=env)
 else:
     a = agent.agent()
 
@@ -16,5 +33,5 @@ def seqActions():
         nSteps = defInput("Insert number of steps: ", nSteps)
         a.nSteps(nSteps,0)
 
-
-seqActions()
+if loop:
+    seqActions()
