a67e3bc1e7f7d11d8e7fe66d965f697e375b742a
daddabarba <davidebarbieri97@gmail.com>
2018-04-16 00:33:49 +0200

    added copy action and policy rutines in LSTM

--- simulation/simModel/agent/learningAgent/ann/LSTM.py
+++ simulation/simModel/agent/learningAgent/ann/LSTM.py
@@ -16,18 +16,24 @@ rnn_bias_key = 'rnn/basic_lstm_cell/bias:0'
 rnn_kernel_key = 'rnn/basic_lstm_cell/kernel:0'
 
 out_weights_key = 'weights'
-out_bias_key = 'bias'
+out_bias_key = 'biases'
 
 
 class LSTM():
+    def restart(self, input_size, rnn_size, output_size, alpha, session=None, scope="lstm"):
+        self = self.__class__(input_size, rnn_size, output_size, alpha, session, scope)
+        return self
+
     def __init__(self, input_size, rnn_size, output_size, alpha, session=None, scope="lstm"):
         # storing scope name
         self.scope = aux.uniqeScope(scope)
 
+        self.alpha = alpha
+
         # setting hyperparameters
         self.input_size = input_size
-        self.output_size = output_size
-        self.rnn_size = rnn_size
+        self.output_size = output_size if type(output_size) != type({}) else len(output_size[out_bias_key])
+        self.rnn_size = rnn_size if type(rnn_size) != type({}) else int(len(rnn_size[rnn_bias_key]) / 4)
 
         # counting epochs
         self.epoch = 1
@@ -49,7 +55,7 @@ class LSTM():
             W = tf.Variable(tf.random_normal([self.rnn_size, self.output_size]))
             b = tf.Variable(tf.random_normal([self.output_size]))
 
-            self.output_layer = {'weights': W, 'biases': b}
+            self.output_layer = {out_weights_key: W, out_bias_key: b}
 
             # saving lstm prediciton and state function (w.r.t. input placeholder)
             (self.prediction, self.state) = self.neural_network_model(self.xPH)
@@ -67,6 +73,18 @@ class LSTM():
 
         self.sess = session
         (self.sess).run(tf.global_variables_initializer())
+
+        if type(output_size) == type({}):
+            self.override(self.output_layer[out_weights_key], output_size[out_weights_key])
+            self.override(self.output_layer[out_bias_key], output_size[out_bias_key])
+
+        if type(rnn_size) == type({}):
+            rnn_size = dict([(self.scope + '/' + name, val) for (name, val) in rnn_size.items()])
+
+            for v in tf.global_variables():
+                if (v.name) in list(rnn_size.keys()):
+                    self.override(v, rnn_size[v.name])
+
 
     # reshapres input tensors to the correct format
     def reshapeData(self, x):
@@ -116,6 +134,75 @@ class LSTM():
         feed_dict = {self.xPH: np.array(self.input_batches)}
 
         return self.sess.run(T, feed_dict)
+
+    def copyOutput(self, ind):
+        new_shape = list(self.sess.run(tf.shape(self.output_layer[out_weights_key])) + [0, 1])
+
+        _w = np.transpose(self.sess.run(self.output_layer[out_weights_key]))[ind]
+        _w = self.sess.run(tf.reshape(_w, [len(_w), 1]))
+
+        new_W = self.changeShape(self.output_layer[out_weights_key], newShape=new_shape)
+        new_W = self.override(new_W, _w, at=[0, new_shape[1] - 1])
+
+        _b = self.sess.run(self.output_layer[out_bias_key])[ind]
+
+        new_b = self.changeShape(self.output_layer[out_bias_key],  newShape=[new_shape[1]])
+        new_b = self.override(new_b, [_b - np.log(2)], [new_shape[1] - 1])
+
+        output_layer = {out_weights_key: self.sess.run(new_W), out_bias_key: self.sess.run(new_b)}
+        rnn_layer = self.getCopy()['rnn']
+
+        self.restart(self.input_size, rnn_layer, output_layer, self.alpha, self.sess, self.scope)
+
+    def getCopy(self):
+
+        W = self.sess.run(self.output_layer[out_weights_key])
+        b = self.sess.run(self.output_layer[out_bias_key])
+
+        output_layer = {out_weights_key: self.sess.run(W), out_bias_key: self.sess.run(b)}
+
+        for v in tf.global_variables():
+            if v.name == self.scope+'/'+rnn_bias_key:
+                rnn_bias = self.sess.run(v)
+            if v.name == self.scope+'/'+rnn_kernel_key:
+                rnn_kernel = v
+
+        rnn_layer = {rnn_kernel_key: rnn_kernel, rnn_bias_key: rnn_bias}
+
+        return {'out': output_layer, 'rnn':rnn_layer}
+
+    def changeShape(self, tensor, newShape):
+        new = tf.Variable(tf.zeros(newShape))
+        self.sess.run(tf.variables_initializer([new]))
+
+        old = np.ndarray.tolist(self.sess.run(tensor))
+
+        new = self.override(new, old)
+        return new
+
+    def override(self, tensor, values, at=None):
+        shape_tensor = list(self.sess.run(tf.shape(tensor)))
+        shape_values = list(self.sess.run(tf.shape(values)))
+
+        if not at:
+            at = np.ndarray.tolist(np.zeros(len(shape_values), dtype=int))
+
+        sup = [range(at[i], at[i] + shape_values[i]) for i in range(len(at))]
+
+        indices = cross.crossProduct(sup)
+
+        val = [self.sess.run(tensor[i]) for i in indices]
+        values = list(self.sess.run(tf.reshape(values, [-1])))
+
+        delta1 = tf.sparse_tensor_to_dense(tf.SparseTensor(indices, val, shape_tensor))
+        delta2 = tf.sparse_tensor_to_dense(tf.SparseTensor(indices, values, shape_tensor))
+
+        result = tensor - delta1 + delta2
+        self.sess.run(result)
+
+        self.sess.run(tensor.assign(result))
+
+        return tensor
 
     def printVars(self):
         vars = self.getVars()
