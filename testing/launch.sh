df1a008513a0f7a7f3fb209a4589739012f7fe82
daddabarba <davidebarbieri97@gmail.com>
2018-09-03 18:26:06 +0200

    when  multiple exp are lunched the name is an argument

--- testing/launch.sh
+++ testing/launch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-n=$(($1 -1))
+n=$(($2 -1))
 
 for experiment in $(seq 0 $n)
 do
-python3 testing_avgR.py name test_$experiment e 1 n 30 maze def &
+python3 testing_avgR.py name $1_$experiment e 1 n 30 maze def &
 done
 echo "Experiments lanched"
