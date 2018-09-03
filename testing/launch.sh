8aa545a55f708fd42852a0418e31d44354ab5903
daddabarba <davidebarbieri97@gmail.com>
2018-09-03 18:41:45 +0200

    each experiment is now lunched in a different terminal so that it can be supervised and stopped

--- testing/launch.sh
+++ testing/launch.sh
@@ -4,6 +4,6 @@ n=$(($2 -1))
 
 for experiment in $(seq 0 $n)
 do
-python3 testing_avgR.py name $1_$experiment e 1 n 30 maze def &
+gnome-terminal --window-with-profile=Bash -- \bash -c "python3 testing_avgR.py name $1_$experiment e 1 n 30 maze def; read"
 done
 echo "Experiments lanched"
