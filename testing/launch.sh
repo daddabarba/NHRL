a00f1603f244e67d16ad6ee31f65efbb7f82c085
daddabarba <davidebarbieri97@gmail.com>
2018-09-04 08:41:53 +0200

    parametrized n timesteps

--- testing/launch.sh
+++ testing/launch.sh
@@ -4,6 +4,6 @@ n=$(($2 -1))
 
 for experiment in $(seq 0 $n)
 do
-gnome-terminal --window-with-profile=Bash -- \bash -c "python3 testing_avgR.py name $1_$experiment e 1 n 30 maze def; read"
+gnome-terminal --window-with-profile=Bash -- \bash -c "python3 testing_avgR.py name $1_$experiment e 1 n $3 maze maze.txt; read"
 done
 echo "Experiments lanched"
